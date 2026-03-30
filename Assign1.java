3d54e450c44e05acf529b2ed70593bc90e06f5f7
尼头车 <llattt1@163.com>
2026-03-30 09:05:25 +0000

    add input test

---
+++ Assign1.java
@@ -0,0 +1,18 @@
+public class Assign1{
+    public static void main(String args[])
+    {
+        int x,y;
+        float z = 3.414f;
+        double w = 3.1415;
+        boolean truth = true;
+        char c;
+        String str;
+        String str1 = "see you again";
+        x=6;
+        y = 1000;
+        c = 'A';
+        str = "give a fuck";
+
+
+    }
+}
