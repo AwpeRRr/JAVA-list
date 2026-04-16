0f51838b2ad869a7fb35a756466d85ef1a6a1779
尼头车 <llattt1@163.com>
2026-04-16 07:23:33 +0000

    new code'overriding'

---
+++ Test project1/OOverriding.java
@@ -0,0 +1,21 @@
+public class OOverriding extends Base{
+    private int i =2;
+    public static void main(String[] args){
+        OOverriding no = new OOverriding();
+        no.increase();
+        System.out.println(no.i);
+        System.out.println(no.getI());
+    }
+    
+}
+
+class Base{
+    private int i =100;
+    public void increase(){
+        this.i ++;
+
+    }
+    public int getI(){
+        return this;
+    }
+}
