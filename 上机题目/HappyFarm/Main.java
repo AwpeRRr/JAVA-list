f184695033df1a744400668c7137e6d984e4e496
尼头车 <llattt1@163.com>
2026-05-12 06:14:35 +0000

    add Chinese

--- 上机题目/HappyFarm/Main.java
+++ 上机题目/HappyFarm/Main.java
@@ -6,7 +6,19 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner s1 = new Scanner(System.in);
 		
-		System.out.println("======== ");
+		System.out.println("======== 简单开心农场管理系统 ========");
+		System.out.println("1. 创建农场对象");
+		System.out.println("2. 根据名称查找农场对象");
+		System.out.println("3. 根据类型输出农场对象");
+		System.out.println("4. 输出指定编号之前的农场对象");
+		System.out.println("5. 照料指定农场对象");
+		System.out.println("6. 判断指定农场对象的类型");
+		System.out.println("7. 收获或移除农场对象");
+		System.out.println("8. 输出所有农场对象");
+		System.out.println("0. 退出系统");
+		System.out.println("请选择操作：");
+
+
 	}
 }
 
