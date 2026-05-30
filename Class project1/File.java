8dddf59120369a400c512facd79242fe5f37cd3e
PRO kJackie <LLATTT1@163.COM>
2026-05-30 19:21:19 +0800

    1

---
+++ Class project1/File.java
@@ -0,0 +1,11 @@
+
+
+public class File {
+    public File(String string, String string2) {
+        //TODO Auto-generated constructor stub
+    }
+    public static void main(String[] args) {
+        File myFile = new File("C:\\Users\\Admin\\Desktop\\file.txt", "student");
+        
+    }
+}
---
+++ Class project1/RenameFile.java
@@ -0,0 +1,7 @@
+public class RenameFile {
+    private static void fileData(File f){
+        System.out.println("File name: " + f.getName());
+        System.out.println("Absolute path:" + f.getAbsolutePath());
+        System.out.println("");
+    }
+}
