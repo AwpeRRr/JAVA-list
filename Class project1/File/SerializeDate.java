6d7289587062c98835e4ff124d4450fa207ea3bc
PRO kJackie <LLATTT1@163.COM>
2026-06-02 23:05:19 +0800

    File prac 1

--- Class project1/File/SerializeDate.java
+++ Class project1/File/SerializeDate.java
@@ -3,12 +3,14 @@ package File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.Date;
+import java.io.FileOutputStream;
 
 public class SerializeDate {
 	SerializeDate(){
 		Date d = new Date();
 		try {
-			ObjectOutputStream s = new ObjectOutputStream("data.ser");
+			ObjectOutputStream s = new ObjectOutputStream(new FileOutputStream("Class project1/File/date.ser"));
 			s.writeObject(d);
 			s.close();
 		}
---
+++ Class project1/File/UnSerializeDate.java
@@ -0,0 +1,26 @@
+package File;
+
+import java.util.Date;
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+
+public class UnSerializeDate {
+	Date d = null;
+	UnSerializeDate(){
+		try {
+			ObjectInputStream s = new ObjectInputStream(new FileInputStream("Class project1/File/date.ser"));
+			Object o = s.readObject();
+			if(d instanceof Date) d = (Date) o;
+			s.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	public static void main(String[] args) {
+		UnSerializeDate un = new UnSerializeDate();
+		System.out.println(un.d.toString());
+	}
+	
+}
